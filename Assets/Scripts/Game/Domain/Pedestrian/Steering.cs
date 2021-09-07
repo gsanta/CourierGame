f260968ab5a198485134cf1db62fefd35b3e4fc7
Sánta Gergely <santagergely90@gmail.com>
2021-09-08 00:55:28 +0200

    Character collision avoidance.

---
+++ Assets/Scripts/Game/Domain/Pedestrian/Steering.cs
@@ -0,0 +1,44 @@
+﻿
+using UnityEngine;
+
+namespace Domain
+{
+    public class Steering
+    {
+        private readonly GameObject gameObject;
+        public float seeAhead = 5f;
+        public float radius = 2f;
+
+        public Steering(GameObject gameObject)
+        {
+            this.gameObject = gameObject;
+        }
+
+        public Vector3 Center { get => gameObject.transform.position; }
+
+        public Vector3 Ahead()
+        {
+            return gameObject.transform.forward * seeAhead;
+        }
+
+        public Vector3 Ahead2()
+        {
+            return gameObject.transform.forward * seeAhead / 2;
+        }
+
+        public float DistToAhead(Steering other)
+        {
+            return Vector3.Distance(Ahead(), other.Center);
+        }
+
+        public float DistToAhead2(Steering other)
+        {
+            return Vector3.Distance(Ahead2(), other.Center);
+        }
+
+        public bool Intersects(Steering other)
+        {
+            return DistToAhead(other) < other.radius || DistToAhead2(other) < other.radius;
+        }
+    }
+}
